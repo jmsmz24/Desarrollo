845892d162f53c7a6861cdd12c0269bbe1d20b1d
jmsmz24 <josem.sanchezm.ccaa@gmail.com>
2020-07-08 17:16:00 +0200

    Cambios en comentarios

--- curso_python.py
+++ curso_python.py
@@ -1,4 +1,4 @@
-#1 PARTE
+#1 PRINCIPIOS BÁSICOS PyQGIS
 #1.1.EXPLORAR CAPA VECTORIAL
 #------------------------------------------------------------------------------
 
@@ -14,40 +14,33 @@ lyr.dataProvider().dataSourceUri()
 lyr.dataProvider().metadata()
 lyr.dataProvider().wkbType()
 
-
 #Obtener todas las entidades con un iterador y guardarlo en una variable llamada features
 features = lyr.getFeatures()
 
 #Convertit en lista la variable features
 features_list = list(features)
 
-#¿Cunto ENP tenemos en españa?
+#¿Cuantas entidades tenemos?
 len(features)
 
 #Obtener el primer elemento de la lista y guardarlo en una variable llamada feature
 feature = features_list[0]
 
-#Obtener el nombre del ENP guardado en la variable feature accedendio a su atributo en el campo sitename
+#Obtener el nombre de la entidad guardada en la variable feature accedendio a su atributo en el campo XXXX
 feature ["sitename"]
 
-
-
-#Obtener la geometria de ENP guardado en la variable feature
+#Obtener la geometria de las entidad guardada en la variable feature
 feature.geometry()
 
-#Listado del nombre de todos los ENP
+#Listado de todss las entidades por el campo XXXX
 [f ["sitename"] for f in features_list]
 
-#Listado de la geometry de todos los ENP
+#Listado de la geometry de todos las entidades
 [f.geometry() for f in features_list]
 
-#Suma del area de todos los ENP
+#Suma del area de todas las entidades
 sum([f ["area_ha"] for f in features_list])
 
-#Suma del area de todos los ENP solo de extremadura
-sum([f ["area_ha"] for f in iface.activeLayer().getFeatures('"ccaa_n_enp"=\'Extremadura\'')])
-sum([f ["area_ha"] for f in iface.activeLayer().getFeatures() if f["ccaa_n_enp"]== "Extremadura"])
-
-
-#Abrir capa
-
+#Suma del area de todas las entidades filtrado a traves de dos métodos
+sum([f ["area_ha"] for f in iface.activeLayer().getFeatures('"ccaa_n_enp"=\'Extremadura\'')]) #Ejemplo bueno
+sum([f ["area_ha"] for f in iface.activeLayer().getFeatures() if f["ccaa_n_enp"]== "Extremadura"]) #Ejemplo malo
