603ee764fd0f49090a95fa0fc2439c1f6183e3c1
jmsmz24 <josem.sanchezm.ccaa@gmail.com>
2020-08-13 19:29:02 +0200

    Se añaden nuevas cosas

--- curso_python.py
+++ curso_python.py
@@ -68,23 +68,37 @@ sum([feature ["area"] for feature in features if feature["NAME_1"]== "Castilla y
 
 #Crear capa de tipo punto y WGS84 llamada layer en memoria virtual
 layer = QgsVectorLayer("Point?crs=ESPG:4326","capa","memory")
-
-#Creamos objeto de tipo campo
 field = QgsField("id", QVariant.String)
-
-#Insertamos campo en capa layer
 layer.dataProvider().addAttributes([field])
 
-#Acualizamos campos
 layer.updateFields()
 
 feature = QgsFeature()
 feature.setFields(layer.fields())
 feature.setAttribute("id","1")
-feature ["id"]
 
 point = QgsPointXY(-3.70256, 40.4165)
+
 geom = QgsGeometry.fromPointXY(point)
+feature.setGeometry(geom)
+layer.dataProvider().addFeatures([feature])
+
+#Crear capa de tipo linea y WGS84 llamada layer en memoria virtual
+
+layer = QgsVectorLayer("LineString?crs=ESPG:4326","capa","memory")
+field = QgsField("id", QVariant.String)
+layer.dataProvider().addAttributes([field])
+
+layer.updateFields()
+feature = QgsFeature()
+feature.setFields(layer.fields())
+feature.setAttribute("id","1")
+
+point1 = QgsPointXY(-3.70256, 40.4165)#Madrid
+point2 = QgsPointXY( -0.906094, 41.655437)#Zaragoza
+point3 = QgsPointXY( 2.15899, 41.38879)#Barcelona
+
+geom = QgsGeometry.fromPolylineXY([point1, point2, point3])
 feature.setGeometry(geom)
 layer.dataProvider().addFeatures([feature])
 
