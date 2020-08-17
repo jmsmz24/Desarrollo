10d750fefdb013de38adf06ed0c7a82707b1b75f
jmsmz24 <josem.sanchezm.ccaa@gmail.com>
2020-08-17 17:14:30 +0200

    Se añaden nuevas cosas

--- ej_1_Sum_agrupar_campo.py
+++ ej_1_Sum_agrupar_campo.py
@@ -12,16 +12,15 @@ totalArea = {}
 #Iterados de cada una de las entidades
 for feature in features:
     #Trabajamos con estos campos
-    ccaa = feature["NAME_1"]#Comunidad Autonoma (CCAA)
+    ccaa = feature["NAME_1"] #Comunidad Autonoma (CCAA)
     area = feature["area"]
     
     #Se rellenan los datos de Comunidad Autonoma y Area en parejas clave-valor ({'ccaa':area}) si el area en mayor de 0
-    if area > 0
-
+    if area > 0:
         #Si en el diccionario totalArea ya existe alguna pareja con la CCAA que se va a insertar, se suma el área al valor de la pareja existente
         if ccaa in totalArea:
             totalArea[ccaa] += area
-        #Si en el diccionario totalArea NO existe ninguna pareja con la CCAA que se va a insertar, se inserta el valor del area
+        #Si en el diccionario totalArea NO existe ninguna pareja con la CCAA que se va a insertar, se inserta la clave (CCAA) y el valor del area.
         else:
             totalArea[ccaa] = area
             
