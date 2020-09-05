e0b1338617e43a40ede12a6d3702cfa7682b5ce1
jmsmz24 <josem.sanchezm.ccaa@gmail.com>
2020-09-05 20:40:34 +0200

    Se añaden nuevas cosas

--- Ejemplos/Extra_DownloadSentinelImage.py
+++ Ejemplos/Extra_DownloadSentinelImage.py
@@ -5,13 +5,14 @@ from datetime import date
 user = QInputDialog().getText(None, "User", "User please:")
 password = QInputDialog().getText(None, "Password", "Password please:")
 
+
 api = SentinelAPI(user, password, 'https://scihub.copernicus.eu/dhus')
 
-
 point = QgsPointXY(-3.70256, 40.4165)
-geom = QgsGeometry.fromPointXY(point) 
+geom = QgsGeometry.fromPointXY(point).asWkt()
 
-products = api.query(geom, platformname = 'Sentinel-2', cloudcoverpercentage = (0, 25),producttype='S2MSI1C',beginPosition='[2020-01-01T00:00:00.000Z TO 2020-02-01T23:59:59.999Z]')
+
+products = api.query(geom, platformname = 'Sentinel-2', cloudcoverpercentage = (0, 25),producttype='S2MSI1C',beginPosition='[2020-01-20T00:00:00.000Z TO 2020-02-01T23:59:59.999Z]')
 json = api.to_geojson(products)
 
 for i in json["features"]:
